002febb98cc8bc3c362bd42362ac505169487b9b
Brendan O'Dea <brendan.ode4@gmail.com>
2018-05-18 22:40:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,12 +2,9 @@ function showRepositories(event, data) {
   //this is set to the XMLHttpRequest object that fired the event
   var repos = JSON.parse(this.responseText)
   console.log(this.responseText)
-  let repoList = "<ul>"
-for(var i=0;i < this.responseText.length; i++) {
-  repoList += "<li>" + this.responseText[i]["name"] + "</li>"
-}
-repoList += "</ul>"
-document.getElementById("repositories").innerHTML = repoList
+  const repoList = `<ul>${repos.map(r => '<li>' + r.name + '</li>').join('')}</ul>
+  
+  document.getElementById("repositories").innerHTML = repoList
 }
 
 function getRepositories() {
